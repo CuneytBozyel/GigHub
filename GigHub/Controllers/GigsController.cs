2df32a9bf9b5ac061cc187d0de9efd7cbc928bb0
Cuneyt B <cuneytbozyel@gmail.com>
2019-01-19 00:43:50 +0300

    Add existing file

---
+++ GigHub/Controllers/GigsController.cs
@@ -0,0 +1,34 @@
+﻿using GigHub.Models;
+using GigHub.ViewModel;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace GigHub.Controllers
+{
+    public class GigsController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public GigsController()
+        {
+            _context = new ApplicationDbContext();
+        }
+        // GET: Gigs
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Authorize]
+        public ActionResult Create()
+        {
+            var viewModel = new GigViewModel
+            {
+                Genres = _context.Genres.ToList()
+            };
+
+            return View(viewModel);
+        }
+    }
+}
---
+++ GigHub/ViewModel/GigViewModel.cs
@@ -0,0 +1,19 @@
+﻿using GigHub.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GigHub.ViewModel
+{
+    public class GigViewModel
+    {
+        public string Venue { get; set; }
+        public string Date { get; set; }
+        public string Time { get; set; }
+        public int Genre { get; set; }
+
+        public IEnumerable<Genre> Genres { get; set; }
+
+    }
+}
