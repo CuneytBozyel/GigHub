af0705773058aa59aedc09df93edd5c2f728b155
Cuneyt B <cuneytbozyel@gmail.com>
2019-01-19 22:19:12 +0300

    Home Index

--- GigHub/Controllers/HomeController.cs
+++ GigHub/Controllers/HomeController.cs
@@ -1,16 +1,27 @@
-﻿using System;
+﻿using GigHub.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Data.Entity;
 
 namespace GigHub.Controllers
 {
     public class HomeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public HomeController()
+        {
+            _context = new ApplicationDbContext();
+        }
         public ActionResult Index()
         {
-            return View();
+            var upComingGigs = _context.Gigs
+                .Include(g => g.Artist)
+                .Include(g=>g.Genre)
+                .ToList();
+            return View(upComingGigs);
         }
 
         public ActionResult About()
